03ea0dc22a395083ee42bfa96deed6c26fad697b
Cookie_Killer361 <jannis1lauerwald@gmail.com>
2023-05-16 22:10:45 +0200

    adding Variables and some datatypes

---
+++ Variable_and._Datastructure.py
@@ -0,0 +1,31 @@
+#Variable and some Datatypes Test
+
+
+#integer
+i=1
+
+#float
+f=3.2
+
+#string
+s="Hello World"
+
+#Booloen
+b=True
+
+#list
+test_list=["apple","banana","cherry","pi"]
+
+#tuple
+test_tuple=("apple","banana","cherry","pi")
+
+#sets
+test_sets={"apple","banana","cherry","pi"}
+
+
+
+print(type(test_list))
+print(type(test_tuple))
+print(type(test_sets))
+
+
