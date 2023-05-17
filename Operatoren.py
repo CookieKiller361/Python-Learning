fd08550e4234b8478be3e136bd02a468ad18442c
Cookie_Killer361 <jannis1lauerwald@gmail.com>
2023-05-17 15:00:28 +0200

    adding Operators

---
+++ Operatoren.py
@@ -0,0 +1,54 @@
+test=2
+test2=1
+
+
+#addieren
+ergebnis=test + test2
+
+#subtrahieren
+ergebnis=test-test2
+
+#multiplizieren
+ergebnis=test*test2
+
+#division
+ergebnis=test/test2
+
+#greater then 
+test>test2
+
+#less then
+test<test2
+
+#greater then or equal
+test>=test2
+
+#less then or equal
+test<=test2
+
+#Modulu
+ergebnis=test%test2
+
+#Exponention
+ergebnis=test**2
+
+#floor division
+ergebnis=test//test2
+
+#not equal
+if test != test2:
+    print("Nicht gleich")
+
+#and 
+if test and test2:
+    print("both of them are True")
+
+#or
+if test or test2:
+    print("one of them are True")
+
+#not 
+if not (test < 5):
+    print("Not")
+
+
