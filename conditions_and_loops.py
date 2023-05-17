25bfc8362e29b0c04686636b39840fabc2ba3c13
Cookie_Killer361 <jannis1lauerwald@gmail.com>
2023-05-17 18:47:17 +0200

    adding conditions and loops

---
+++ conditions_and_loops.py
@@ -0,0 +1,40 @@
+test=2
+test2=1
+
+#if 
+if test != test2:
+    print("test not equal test2")
+
+#if-else
+if test == test2:
+    print("test not equal test2")
+else:
+    print("test equal test2")
+
+#if-elif-else
+if test == test2:
+    print("test not equal test2")
+elif test >= test2:
+    print("test is greater then test2")
+else:
+    print("test equal test2")
+
+
+word="test"
+#for loop
+for characters in word:
+    print("test")
+
+#while
+while test >= test2:
+    print("test is greater then test2")
+    test2+=1
+
+#do-while
+number=0
+
+while True:
+    print(number)
+    number+=1
+    if not number <=10:
+        break
