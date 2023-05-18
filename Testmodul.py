35618dfad4108a65797e374c1ffbeaba2db5462f
Cookie_Killer361 <jannis1lauerwald@gmail.com>
2023-05-18 16:56:30 +0200

    adding functions and modules

---
+++ Testmodul.py
@@ -0,0 +1,2 @@
+def output():
+    print("Great this works")
---
+++ functions_and_modules.py
@@ -0,0 +1,14 @@
+#function 
+def test(text):
+    print("this is a test from inside a function")
+    print(text)
+
+test("Test")
+
+#modul
+import Testmodul # normally at the top of the file 
+
+Testmodul.output()
+
+
+
