04fac14c8b9f48895a2826f003aa647fdce91f91
Cookie_Killer361 <jannis1lauerwald@gmail.com>
2023-05-19 13:26:39 +0200

    adding standard Datastructures

---
+++ Standard_Datastructure.py
@@ -0,0 +1,21 @@
+#set
+testset={1,1,2}
+print(testset)
+
+#tuple
+testtuple=(1,5,3,1)
+print(testtuple)
+
+#list
+testliste=[1,1,2,4,5,9,3]
+print(testliste)
+
+#dictionarie
+testdictionarie={
+
+"test" : "testübersetzung",
+"test2" : "test2übersetzung"
+
+}
+
+print(testdictionarie)
