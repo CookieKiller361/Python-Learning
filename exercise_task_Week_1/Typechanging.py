58dfaa7c93c9579064a5066594824c381e346dc5
Cookie_Killer361 <jannis1lauerwald@gmail.com>
2023-05-20 16:13:31 +0200

    adding exercise task: Typechanging

---
+++ exercise_task_Week_1/Typechanging.py
@@ -0,0 +1,34 @@
+def changetype(choosenvariable, changein):
+  choosen={
+    1:int(choosenvariable),
+    2:float(choosenvariable),
+    3:str(choosenvariable)
+  }
+  return choosen.get(changein)
+
+
+
+
+x=10
+y="Hello"
+z=10.56
+
+#print the question
+print(f'1. {x} is {type(x)}\n2. {y} is {type(y)}\n3. {z} is {type(z)}')
+choosenvariable=input('\nchange data type: ')
+
+#Reference to the variables
+if choosenvariable==1:
+  choosenvariable=x
+  print(choosenvariable)
+elif choosenvariable==2:
+  choosenvariable=y
+elif choosenvariable==3:
+  choosenvariable=z
+#print question 2
+changein=input('\nchange datatype in\n\n1. inteterger\n2. float\n3. string\nchose a number: ')
+print(changetype(choosenvariable, int(changein)))
+
+
+
+    
