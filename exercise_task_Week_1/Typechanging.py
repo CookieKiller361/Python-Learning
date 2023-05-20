c717c4dbaa48082476e79032c38b3a6ea9391a3c
Cookie_Killer361 <jannis1lauerwald@gmail.com>
2023-05-20 16:50:41 +0200

    Fixing Bug in Typechanging: were the choosenvariable aren't refer to the variable

--- exercise_task_Week_1/Typechanging.py
+++ exercise_task_Week_1/Typechanging.py
@@ -1,4 +1,5 @@
 def changetype(choosenvariable, changein):
+  changein=changein
   choosen={
     1:int(choosenvariable),
     2:float(choosenvariable),
@@ -18,13 +19,13 @@ print(f'1. {x} is {type(x)}\n2. {y} is {type(y)}\n3. {z} is {type(z)}')
 choosenvariable=input('\nchange data type: ')
 
 #Reference to the variables
-if choosenvariable==1:
+if choosenvariable=='1':
   choosenvariable=x
-  print(choosenvariable)
-elif choosenvariable==2:
+elif choosenvariable=='2':
   choosenvariable=y
-elif choosenvariable==3:
+elif choosenvariable=='3':
   choosenvariable=z
+
 #print question 2
 changein=input('\nchange datatype in\n\n1. inteterger\n2. float\n3. string\nchose a number: ')
 print(changetype(choosenvariable, int(changein)))
